682cdcdcd0f0462a7501468b9d34d86727fb2186
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-06-25 16:48:25 -0700

    Update tests

---
+++ tests/test_noise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""Tests for `xcorr` package."""
+
+
+import unittest
+
+from xcorr import xcorr
+
+
+class Interferometer(unittest.TestCase):
+    """Tests for `xcorr` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+
+class Mapper(unittest.TestCase):
+    """Tests for `xcorr` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
--- tests/test_xcorr.py
+++ tests/test_xcorr.py
@@ -8,7 +8,59 @@ import unittest
 from xcorr import xcorr
 
 
-class TestXcorr(unittest.TestCase):
+class Cube(unittest.TestCase):
+    """Tests for `xcorr` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+
+class CarbonMonoxide(unittest.TestCase):
+    """Tests for `xcorr` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+
+class HAlpha(unittest.TestCase):
+    """Tests for `xcorr` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+
+class LymanAlpha(unittest.TestCase):
+    """Tests for `xcorr` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+
+class Hyperfine(unittest.TestCase):
     """Tests for `xcorr` package."""
 
     def setUp(self):
