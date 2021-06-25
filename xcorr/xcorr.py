77bc7679360b08c7879f8d891481fe2b4bc7f417
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-06-25 16:11:53 -0700

    Throw together skeleton code

--- xcorr/xcorr.py
+++ xcorr/xcorr.py
@@ -1,1 +1,43 @@
-"""Main module."""
+import numpy as np
+import powerbox
+import scipy
+
+
+class Cube:
+    """ """
+
+    def __init__(self, **kwargs):
+        """ """
+        self.kwargs = kwargs
+
+    def cross(self, a):
+        """
+        Cross-correlate with another cube object
+        """
+        pass
+
+    def pspec():
+        """
+        Calculate the power spectrum
+        """
+        pass
+
+
+class Hyperfine(Cube):
+    """ """
+
+    def __init__(self, **kwargs):
+        """ """
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        """ """
+        pass
+
+
+class LymanAlpha(Cube):
+    """ """
+
+    def __init__(self):
+        """ """
+        super().__init__(**kwargs)
