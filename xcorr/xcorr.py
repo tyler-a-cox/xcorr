0fa1ea3b356400f614233f96280df2d0e29986a3
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-06-28 16:01:43 -0700

    Remove old import

--- xcorr/xcorr.py
+++ xcorr/xcorr.py
@@ -10,7 +10,6 @@ from astropy.cosmology import Planck15 as cosmo
 from astropy import constants as const
 from powerbox import get_power
 from hmf import MassFunction
-from xcorr import load_binary_data
 from scipy.ndimage.filters import gaussian_filter
 
 
