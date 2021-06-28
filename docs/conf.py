c925d6b4630843a0d0abdc59d39e05712b3c26e5
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-06-28 16:12:26 -0700

    Fix indent error

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,15 @@ sys.path.insert(0, os.path.abspath(".."))
 import xcorr
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = [
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "matplotlib.pyplot",
+    "scipy.interpolate",
+]
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
